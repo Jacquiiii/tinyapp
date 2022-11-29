44466973d583075fc5d21bb7d23b2257e928efd5
Jacquiiii <jacqui.koroll@telus.com>
2022-11-28 17:50:20 -0800

    added comments to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,10 +1,16 @@
+// function to generate a random 6 digit alphanumeric number to be used in short url
+const generateRandomString = () => {
+  const randomNumber = Math.random();
+  const numberInBase36 = randomNumber.toString(36);
+  const shortString = numberInBase36.substring(2, 8);
+  return shortString;
+};
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +21,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// route to display data in urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -23,15 +30,18 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// route to display all urls from urlDatabase object
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// route to form for user to create new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// route to individual url based on id
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -40,18 +50,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// receives form submission from '/urls/new'
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
+  console.log(req.body); 
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-const generateRandomString = () => {
-  const randomNumber = Math.random();
-  const numberInBase36 = randomNumber.toString(36);
-  const shortString = numberInBase36.substring(2, 8);
-  return shortString;
-};
