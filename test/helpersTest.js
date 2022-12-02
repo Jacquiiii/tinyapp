04c54403d136382f9fd8ff34186c41ef317dbfbd
Jacquiiii <jacqui.koroll@telus.com>
2022-12-01 22:49:21 -0800

    created test folder with helpersTest.js to test helper function

---
+++ test/helpersTest.js
@@ -0,0 +1,35 @@
+// test code for getUserByEmail
+
+const { assert } = require('chai');
+const { getUserByEmail } = require('../helpers.js');
+
+
+// test code for getUserByEmail
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+
+describe('getUserByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user, expectedUserID);
+  });
+
+  it('should return undefined for invalid email', function() {
+    const user = getUserByEmail("harrypotter123@goldensnitch.com", testUsers)
+    assert.equal(user, undefined);
+  });
+
+});
